e010b07113abaee13da477a981b0eb3c75c5dad0
Lucas Felipe Costa <lucasfelipecost4@hotmail.com>
2021-07-13 23:18:56 -0300

    Salvando cards

--- app/src/main/java/br/com/lfcapp/cartodevisita/ui/AddCardActivity.kt
+++ app/src/main/java/br/com/lfcapp/cartodevisita/ui/AddCardActivity.kt
@@ -3,8 +3,11 @@ package br.com.lfcapp.cartodevisita.ui
 import android.content.DialogInterface
 import android.content.res.ColorStateList
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import br.com.lfcapp.cartodevisita.App
 import br.com.lfcapp.cartodevisita.R
+import br.com.lfcapp.cartodevisita.data.Cards
 import br.com.lfcapp.cartodevisita.databinding.ActivityAddCardBinding
 import br.com.lfcapp.cartodevisita.util.Uteis
 import com.flask.colorpicker.ColorPickerView
@@ -14,7 +17,10 @@ import com.flask.colorpicker.builder.ColorPickerDialogBuilder
 
 class AddCardActivity : AppCompatActivity() {
     private val binding by lazy { ActivityAddCardBinding.inflate(layoutInflater) }
-    var corSelecionada: Int = -1
+    private var corSelecionada: Int = -1
+    private val mainViewModel: MainViewModel by viewModels {
+        MainViewModelFactory((application as App).repository)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -49,6 +55,20 @@ class AddCardActivity : AppCompatActivity() {
                 })
                 .build()
                 .show()
+        }
+
+        binding.btnConfirma.setOnClickListener {
+            val card = Cards(
+                nome = binding.tilName.editText?.text.toString(),
+                empresa = binding.tilEmpresa.editText?.text.toString(),
+                email = binding.tilEmail.editText?.text.toString(),
+                telefone = binding.tilTelefone.editText?.text.toString(),
+                cor = corSelecionada
+            )
+            mainViewModel.insert(card)
+            Uteis.toast(this, getString(R.string.label_show_sucesso))
+            finish()
+
         }
     }
 }
